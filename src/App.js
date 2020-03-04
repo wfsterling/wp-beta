d605e4eefe5d06c6e5df7feeab32b72b350a8eb5
William Sterling <sterlinw@b19945.local>
2020-03-03 20:25:07 -0700

    Working integration MM, 3Box, Material UI

--- src/App.js
+++ src/App.js
@@ -1,40 +1,46 @@
 import React from 'react';
-//import NavBar from './components/NavBar';
-//import Button from '@material-ui/core/Button';
-import Fortmatic from 'fortmatic';
-
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Boxload from './components/Boxload';
 
-function App() {
-  const fmPhantom = new Fortmatic.Phantom('pk_test_8C80A8E135F3BBCD');
-  const handleLoginWithMagicLink = async () => {
-    const email = document.getElementById('user-email').value;
-    
-    const user = await fmPhantom.loginWithMagicLink({ 
-      email,
-      showUI: false  
-    });
-    console.log(await user.isLoggedIn()); // => true
-    console.log((await user.getMetadata()).publicAddress); // You should use this as a unique user Id.
-  };
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    marginLeft: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
   
-
   return (
-    
-    <div>
-      <div className="header"></div>
-      <h2>Administration Screen</h2>
-      <p>This screen allows you to onboard new users onto new tokens, or to replace lost or stolen tokens.</p>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+        <img src={require('./images/wolf-pack-white.png')} alt="WolfPack Logo of two wolves" className="wolf-logo-header"/>
+        <Boxload/>
+        {/* <Button color="inherit" className="login-btn-header">Login</Button> */}
+        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        </Toolbar>
+      </AppBar>
       
-      
-      
-      <input type="text" id="user-email" placeholder="Enter your email!" />
-      <button variant="contained" color="primary" onClick={handleLoginWithMagicLink}>
-        Hello World
-      </button>
-
     </div>
   );
 }
-
-export default App;
