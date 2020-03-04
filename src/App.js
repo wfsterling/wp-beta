0642290d06327066fab7483b1ef6e7eda12f4523
William Sterling <sterlinw@b19945.local>
2020-03-03 21:38:11 -0700

    updaing app

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Boxload from './components/Boxload';
