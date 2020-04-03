ed08a29908175b667c8b36e4d79239deeb2db8cb
DaskJeeves <daskjeeves@gmail.com>
2020-04-02 22:38:49 -0600

    Added material styles to create work item

--- src/components/pages/CreateWorkItem/CreateWorkItem.js
+++ src/components/pages/CreateWorkItem/CreateWorkItem.js
@@ -2,13 +2,24 @@ import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import './CreateWorkItem.scss';
 import moment from "moment";
+import {
+    MuiPickersUtilsProvider,
+    KeyboardTimePicker,
+    KeyboardDatePicker,
+} from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
 
 function CreateWorkItem(props) {
 
+    console.log(moment().startOf('day').format('YYYY-MM-DD HH:mm'));
+
     const [newWorkItem, setNewWorkItem] = useState({
         date: moment().format('YYYY-MM-DD'),
-        startTime: '12:00',
-        endTime: '16:00'
+        startTime:  moment().startOf('day').add(12, 'h').format('YYYY-MM-DD HH:mm'),
+        endTime:  moment().startOf('day').add(16, 'h').format('YYYY-MM-DD HH:mm')
     });
 
     const setNewWorkItemAttribute = (name, value) => {
@@ -20,19 +31,48 @@ function CreateWorkItem(props) {
     return (<div className="container">
         <div className="create-item">
             <div className='inputs-flex'>
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                    <div className='work-input'>
+                        <KeyboardDatePicker
+                            disableToolbar
+                            variant="inline"
+                            format="MM/dd/yyyy"
+                            margin="normal"
+                            id="date-picker-inline"
+                            label="Date"
+                            value={newWorkItem.date}
+                            onChange={date => setNewWorkItemAttribute('date', date)}
+                            KeyboardButtonProps={{
+                                'aria-label': 'change date',
+                            }}
+                        />
+                    </div>
+                    <div className='work-input'>
+                        <KeyboardTimePicker
+                            margin="normal"
+                            id="time-picker"
+                            label="Start Time"
+                            value={newWorkItem.startTime}
+                            onChange={time => setNewWorkItemAttribute('startTime', time)}
+                        />
+                    </div>
+                    <div className='work-input'>
+                        <KeyboardTimePicker
+                            margin="normal"
+                            id="time-picker"
+                            label="End Time"
+                            value={newWorkItem.endTime}
+                            onChange={time => setNewWorkItemAttribute('endTime', time)}
+                        />
+                    </div>
+                </MuiPickersUtilsProvider>
                 <div className='work-input'>
-                    Date: <input type='date' value={newWorkItem.date} onChange={e => setNewWorkItemAttribute('date', e.target.value)}/>
+                    <TextField id="standard-basic" label="Cost (USD)" onChange={e => setNewWorkItemAttribute('rate', e.target.value)}/>
                 </div>
-                <div className='work-input'>
-                    Start Time: <input type='time' value={newWorkItem.startTime} onChange={e => setNewWorkItemAttribute('startTime', e.target.value)}/>
-                </div>
-                <div className='work-input'>
-                    End Time: <input type='time' value={newWorkItem.endTime} onChange={e => setNewWorkItemAttribute('endTime', e.target.value)}/>
-                </div>
-                <div className='work-input'>
-                    Rate: <input onChange={e => setNewWorkItemAttribute('rate', e.target.value)}/>
-                </div>
-                <button onClick={() => props.saveWorkItem(newWorkItem)}>Save</button>
+                <br/>
+                <Button variant="contained" color="primary" onClick={() => props.saveWorkItem(newWorkItem)}>
+                    SAVE
+                </Button>
             </div>
         </div>
     </div>)
